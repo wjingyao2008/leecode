874a5278165f8183a8d5724b36e24c47c623b65f
yangyang01 <yangyang01@cmii.chinamobile.com>
2023-03-02 16:25:04 +0800

    add some leetcode to start

---
+++ src/test/java/com/example/leecode/YangApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.leecode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YangApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+	}
+
+}
